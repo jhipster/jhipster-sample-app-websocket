04dc42b954a898a50b8975b85ee8de0d0ea88fe8
Julien Dubois <julien.dubois@gmail.com>
2015-08-05 22:08:08 +0200

    initial commit

---
+++ src/main/java/com/mycompany/myapp/web/websocket/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Access Objects used by WebSocket services.
+ */
+package com.mycompany.myapp.web.websocket.dto;
---
+++ src/main/java/com/mycompany/myapp/web/websocket/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * WebSocket services, using Spring Websocket.
+ */
+package com.mycompany.myapp.web.websocket;
