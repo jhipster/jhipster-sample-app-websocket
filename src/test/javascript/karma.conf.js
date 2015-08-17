23174fd6c887f8cf147c0edfebe08bc8d59d1808
Julien Dubois <julien.dubois@gmail.com>
2015-08-17 23:51:32 +0200

    automatic project update

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -53,12 +53,13 @@ module.exports = function (config) {
         reporters: ['dots', 'jenkins', 'coverage', 'progress'],
 
         jenkinsReporter: {
+            
             outputFile: '../target/test-results/karma/TESTS-results.xml'
         },
 
         coverageReporter: {
+            
             dir: '../target/test-results/coverage',
-
             reporters: [
                 {type: 'lcov', subdir: 'report-lcov'}
             ]
