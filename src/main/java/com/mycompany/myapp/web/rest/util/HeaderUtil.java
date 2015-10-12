973c56083019f71a68afbeabc94a432f6cf46bab
Julien Dubois <julien.dubois@gmail.com>
2015-10-12 16:28:39 +0200

    automatic project update

--- src/main/java/com/mycompany/myapp/web/rest/util/HeaderUtil.java
+++ src/main/java/com/mycompany/myapp/web/rest/util/HeaderUtil.java
@@ -7,14 +7,14 @@ import org.springframework.http.HttpHeaders;
  *
  */
 public class HeaderUtil {
- 
+
     public static HttpHeaders createAlert(String message, String param) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-samplewebsocketApp-alert", message);
         headers.add("X-samplewebsocketApp-params", param);
         return headers;
     }
-    
+
     public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
         return createAlert("samplewebsocketApp." + entityName + ".created", param);
     }
@@ -26,5 +26,4 @@ public class HeaderUtil {
     public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
         return createAlert("samplewebsocketApp." + entityName + ".deleted", param);
     }
-
 }
