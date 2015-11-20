561a956560bc12873d73d9a8e967d8313c569cd4
Julien Dubois <julien.dubois@gmail.com>
2015-11-20 16:13:55 +0100

    automatic project update

--- src/main/webapp/scripts/app/admin/metrics/metrics.controller.js
+++ src/main/webapp/scripts/app/admin/metrics/metrics.controller.js
@@ -49,7 +49,7 @@ angular.module('sampleWebsocketApp')
                     size: 'lg',
                     resolve: {
                         threadDump: function() {
-                            return data;
+                            return data.content;
                         }
 
                     }
