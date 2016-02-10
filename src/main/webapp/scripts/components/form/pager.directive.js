184a6e4a2c21b23b46f0807d9269c69031dd1890
Julien Dubois <julien.dubois@gmail.com>
2016-02-10 10:34:02 +0100

    automatic project update

--- src/main/webapp/scripts/components/form/pager.directive.js
+++
@@ -1,9 +0,0 @@
-/* globals $ */
-'use strict';
-
-angular.module('sampleWebsocketApp')
-    .directive('sampleWebsocketAppPager', function() {
-        return {
-            templateUrl: 'scripts/components/form/pager.html'
-        };
-    });
