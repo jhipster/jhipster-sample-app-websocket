0fe16430271f95cc43c05e834ac648dda218b969
Julien Dubois <julien.dubois@gmail.com>
2018-07-12 16:05:14 +0200

    automatic project update

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -63,7 +63,12 @@ module.exports = (options) => webpackMerge(commonConfig({ env: ENV }), {
             test: /\.ts$/,
             use: [
                 { loader: 'angular2-template-loader' },
-                { loader: 'cache-loader' },
+                {
+                    loader: 'cache-loader',
+                    options: {
+                      cacheDirectory: path.resolve('target/cache-loader')
+                    }
+                },
                 {
                     loader: 'thread-loader',
                     options: {
