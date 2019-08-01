dd7ded696de0bd9c55dbfa617ff4a6f59a2763f8
Pascal Grimaud <pascalgrimaud@gmail.com>
2019-08-01 09:33:26 +0200

    Initial application generated by JHipster-6.2.0

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
       [
         {
           path: 'admin',
-          loadChildren: './admin/admin.module#JhipsterWebsocketSampleApplicationAdminModule'
+          loadChildren: () => import('./admin/admin.module').then(m => m.JhipsterWebsocketSampleApplicationAdminModule)
         },
         ...LAYOUT_ROUTES
       ],
