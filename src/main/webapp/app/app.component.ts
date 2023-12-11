a7daab2abe9224a0aa48fc4ecb488dcfc16e7358
Deepu <d4udts@gmail.com>
2023-12-11 13:15:05 +0100

    automatic project update for v8.1.0

---
+++ src/main/webapp/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/en';
+import dayjs from 'dayjs/esm';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+
+import { ApplicationConfigService } from 'app/core/config/application-config.service';
+import { fontAwesomeIcons } from './config/font-awesome-icons';
+import MainComponent from './layouts/main/main.component';
+import { TrackerService } from './core/tracker/tracker.service';
+
+@Component({
+  selector: 'jhi-app',
+  standalone: true,
+  template: '<jhi-main></jhi-main>',
+  imports: [
+    MainComponent,
+    // jhipster-needle-angular-add-module JHipster will add new module here
+  ],
+})
+export default class AppComponent {
+  constructor(
+    applicationConfigService: ApplicationConfigService,
+    iconLibrary: FaIconLibrary,
+    trackerService: TrackerService,
+    dpConfig: NgbDatepickerConfig,
+  ) {
+    trackerService.setup();
+    applicationConfigService.setEndpointPrefix(SERVER_API_URL);
+    registerLocaleData(locale);
+    iconLibrary.addIcons(...fontAwesomeIcons);
+    dpConfig.minDate = { year: dayjs().subtract(100, 'year').year(), month: 1, day: 1 };
+  }
+}
